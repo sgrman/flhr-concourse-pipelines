94fad5e437e10e1b03e0997303d42d8831cfd448
Scottie Ray <scottieray@sray.local>
2019-07-16 18:50:15 -0500

    updated nsx-setup

--- nsxt-setup.sh
+++ nsxt-setup.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 # EDIT names and domain
-CONCOURSE_ENDPOINT=fly.vballin.com:8080
+CONCOURSE_ENDPOINT=fly.flhrnet.local:8080
 CONCOURSE_TARGET=nsx-concourse
 PIPELINE_NAME=install-nsx-t
-CONCOURSE_USER=nsx
+CONCOURSE_USER=admin
 CONCOURSE_PW=vmware
 
-alias fly-s="fly -t $CONCOURSE_TARGET set-pipeline -p $PIPELINE_NAME -c ~/repos/nsx-t-datacenter-ci-pipelines/pipelines/nsx-t-install.yml -l nsbu-nsx-t-params.yml"
+alias fly-s="fly -t $CONCOURSE_TARGET set-pipeline -p $PIPELINE_NAME -c ../nsx-t-datacenter-ci-pipelines/pipelines/nsx-t-install.yml -l nsx-t-params.yml"
 alias fly-d="fly -t $CONCOURSE_TARGET destroy-pipeline -p $PIPELINE_NAME"
 alias fly-l="fly -t $CONCOURSE_TARGET containers | grep $PIPELINE_NAME"
 alias fly-h="fly -t $CONCOURSE_TARGET hijack -b "
